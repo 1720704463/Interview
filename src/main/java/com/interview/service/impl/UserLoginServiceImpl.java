a6a94d0d91fe942a6e6126865c7984a1ed7c08d3
rxliuli <rxliuli@gmail.com>
2018-02-27 20:19:59 +0800

    删除了 UserLoginServiceImpl 中的手动注入的 UserLoginMapper 实例,作为代替,使用继承 ServiceImpl 中已经自动注入的 baseMapper 来操作子类

--- src/main/java/com/interview/service/impl/UserLoginServiceImpl.java
+++ src/main/java/com/interview/service/impl/UserLoginServiceImpl.java
@@ -5,7 +5,6 @@ import com.interview.dao.UserLoginMapper;
 import com.interview.entity.UserLogin;
 import com.interview.service.UserLoginService;
 import com.interview.util.EncryptUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,15 +12,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserLoginServiceImpl extends ServiceImpl<UserLoginMapper, UserLogin> implements UserLoginService {
-  /**
-   * 用户数据库访问接口对象
-   */
-  @Autowired
-  private UserLoginMapper userLoginMapper;
-
   @Override
   public UserLogin getByUserLogin(UserLogin userLogin) {
     userLogin.setPassword(EncryptUtil.sha512Hex(userLogin.getPassword()));
-    return userLoginMapper.selectOne(userLogin);
+    return baseMapper.selectOne(userLogin);
   }
 }
