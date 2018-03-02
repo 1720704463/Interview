d57151b1c47d587a7c7fb8d26bcb279cd7f26821
rxliuli <rxliuli@gmail.com>
2018-03-02 16:47:34 +0800

    将添加一场新的考试从 CommonRestController(公共 Rest 控制器) 中移至 UserController(用户相关控制器) 下面

--- src/main/java/com/interview/web/CommonRestController.java
+++ src/main/java/com/interview/web/CommonRestController.java
@@ -81,14 +81,4 @@ public class CommonRestController {
     return JsonResult.getSuccess(topicTypeService.selectList(new EntityWrapper<>()));
   }
 
-  /**
-   * 随机获取指定数量的面试题目列表
-   */
-  @RequestMapping(path = "/getTopicRandom")
-  public JsonResult<List<Topic>> getTopicRandom(
-    @RequestParam(required = false) Integer topicNumber
-  ) {
-    List<Topic> topicList = topicService.listByRandom(topicNumber);
-    return JsonResult.getSuccess(topicList);
-  }
 }
--- src/main/java/com/interview/web/UserController.java
+++ src/main/java/com/interview/web/UserController.java
@@ -1,17 +1,22 @@
 package com.interview.web;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.interview.entity.UserLogin;
-import com.interview.service.UserLoginService;
-import com.interview.util.ConstantsUtil;
-import com.interview.util.EncryptUtil;
-import com.interview.util.JsonResult;
+import com.google.gson.reflect.TypeToken;
+import com.interview.entity.*;
+import com.interview.service.*;
+import com.interview.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author rxliuli
@@ -21,6 +26,14 @@ import javax.servlet.http.HttpSession;
 public class UserController {
   @Autowired
   private UserLoginService userLoginService;
+  @Autowired
+  private TopicService topicService;
+  @Autowired
+  private ExamService examService;
+  @Autowired
+  private ResultService resultService;
+  @Autowired
+  private ExamTopicService examTopicService;
 
   /**
    * 跳转到登陆
@@ -125,5 +138,74 @@ public class UserController {
   @RequestMapping(path = "/userTestPage")
   public String userTestPage() {
     return "foreground/userTestPage";
+  }
+
+  /**
+   * 提交用户测验
+   */
+  @RequestMapping(path = "/submitUserTest")
+  @ResponseBody
+  public JsonResult<?> submitUserTest(
+    @RequestParam(required = false) String topicListString,
+    @RequestParam(required = false) Long examId,
+    HttpSession session
+  ) {
+    //获取全部的题目完成情况
+    List<Topic> topicList = GsonUtil.gsonToList(topicListString, new TypeToken<List<Topic>>() {
+    });
+    UserLogin userLogin = (UserLogin) session.getAttribute(ConstantsUtil.USER_SESSION);
+    //根据编号查询考试
+    Exam exam = examService.selectOne(new EntityWrapper<Exam>().eq("id", examId));
+    Result result = new Result(
+      userLogin.getId(),
+      examId,
+      0D,
+      exam.getStartTime(),
+      new Timestamp(System.currentTimeMillis())
+    );
+    //添加考试
+    boolean resultBoo = resultService.insert(result);
+    List<ExamTopic> examTopicList = topicList.stream()
+      .map(topic -> new ExamTopic(result.getId(), topic.getId(), 0D, topic.getAnswer()))
+      .collect(Collectors.toList());
+    //添加答案
+    boolean examTopicListBoo = examTopicService.insertBatch(examTopicList);
+    return new JsonResult<>().setSuccess(resultBoo && examTopicListBoo);
+  }
+
+  /**
+   * 添加一场测试
+   * 随机获取指定数量的面试题目列表
+   * 存入考试表
+   */
+  @RequestMapping(path = "/addExamUserTest")
+  @ResponseBody
+  public JsonResult<Map<String, Object>> addExamUserTest(
+    @RequestParam(required = false) Integer topicNumber
+  ) {
+    //获取指定数量的面试题目
+    List<Topic> topicList = topicService.listByRandom(topicNumber);
+    //获取面试题目 id 字符串
+    List<Long> idList = topicList.stream()
+      .map(Topic::getId)
+      .collect(Collectors.toList());
+    String topicIds = TopicIdsUtil.join(idList);
+    //添加一场考试
+    long nowTime = System.currentTimeMillis();
+    Exam exam = new Exam(
+      "用户测试 ",
+      new Timestamp(nowTime),
+      //一小时之后的时间
+      new Timestamp(nowTime + 60_000),
+      topicIds
+    );
+    boolean boo = examService.insert(exam);
+    if (!boo) {
+      return JsonResult.getError("添加新的考试错误了,请尝试刷新页面重试!");
+    }
+    Map<String, Object> map = new HashMap<>(2);
+    map.put("topicList", topicList);
+    map.put("examId", exam.getId());
+    return JsonResult.getSuccess(map);
   }
 }
