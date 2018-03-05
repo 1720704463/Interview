198bb71882927bef9603298cb16e27fbf4d98454
rxliuli <rxliuli@gmail.com>
2018-03-05 11:27:57 +0800

    删除了 home.js 的一些无用代码,并且将获取窗口高度等代码封装道 commonHeader.js 中

--- src/main/webapp/statics/foreground/js/home.js
+++ src/main/webapp/statics/foreground/js/home.js
@@ -83,15 +83,6 @@ $(function () {
   }
 
   /**
-   * 加载首页
-   */
-  $("button.start").click(function () {
-    pageIndex = 1;
-    $(".topicList .topic").remove();
-    changePage()
-  });
-
-  /**
    * 实时搜索功能.
    */
   $("#searchTopicTitle").on("input", function () {
@@ -125,30 +116,3 @@ $(function () {
 
 
 });
-
-/*取窗口可视范围的高度*/
-function getClientHeight() {
-  var clientHeight;
-  if (document.body.clientHeight && document.documentElement.clientHeight) {
-    clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
-  } else {
-    clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
-  }
-  return clientHeight;
-}
-
-/*取窗口滚动条高度*/
-function getScrollTop() {
-  var scrollTop = 0;
-  if (document.documentElement && document.documentElement.scrollTop) {
-    scrollTop = document.documentElement.scrollTop;
-  } else if (document.body) {
-    scrollTop = document.body.scrollTop;
-  }
-  return scrollTop;
-}
-
-/*取文档内容实际高度*/
-function getScrollHeight() {
-  return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
-}
